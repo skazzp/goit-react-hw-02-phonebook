c77386c092d0240b6d3aba77b78c6a97635fe7aa
skazzp <skazzp@gmail.com>
2022-09-15 22:36:09 +0300

    p6 ready

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import css from './ContactList.module.css';
+const ContactList = ({ filteredContacts, removeContact }) => {
+  return (
+    <ul className={css.list}>
+      {filteredContacts.map(contact => {
+        return (
+          <li key={contact.id} className={css.item}>
+            <span>{contact.name}: </span>
+            <span>{contact.number}</span>
+            <button type="button" onClick={() => removeContact(contact.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,16 @@
+import css from './Filter.module.css';
+const Filter = ({ setFilter, filterState }) => {
+  return (
+    <label htmlFor="filter">
+      <p className={css.label}>Find contacts by name</p>
+      <input
+        type="text"
+        name="filter"
+        value={filterState}
+        onChange={setFilter}
+      />
+    </label>
+  );
+};
+
+export default Filter;
